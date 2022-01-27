2f709a428e3b7a425fbfbe25822714cf52d457a4
rasel <russelroy15@gmail.com>
2022-01-28 02:05:28 +0600

    WIP on Redux typescript project

--- src/components/ReservationCart.tsx
+++ src/components/ReservationCart.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { v4 as uuid } from "uuid";
+import { addCustomer } from "../features/customerSlice";
 import { removeReservation } from "../features/reservationSlice";
 
 interface ReservationCartTypes {
@@ -14,7 +16,16 @@ export default function ReservationCart({ name, index }: ReservationCartTypes) {
     <div className="reservation-card-container">
       {name}
       <button
-        onClick={() => dispatch(removeReservation(index))}
+        onClick={() => {
+          dispatch(removeReservation(index));
+          dispatch(
+            addCustomer({
+              id: uuid(),
+              name,
+              food: [],
+            })
+          );
+        }}
         style={{ padding: "3px 6px", margin: "0 10px" }}
       >
         Add Cart
