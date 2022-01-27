4dd2e22512c4ef1eeebaa37d998827f617b53d6d
rasel <russelroy15@gmail.com>
2022-01-27 18:45:49 +0600

    WIP on restaurant project

--- src/features/reservationSlice.ts
+++ src/features/reservationSlice.ts
@@ -1,13 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-const initialState = {
+interface ReservationsState {
+  value: string[];
+}
+
+const initialState: ReservationsState = {
   value: [],
 };
 
 export const reservationsSlice = createSlice({
   name: "reservations",
   initialState,
-  reducers: {},
+  reducers: {
+    addReservation: (state, action: PayloadAction<string>) => {
+      state.value.push(action.payload);
+    },
+  },
 });
 
 export default reservationsSlice.reducer;
