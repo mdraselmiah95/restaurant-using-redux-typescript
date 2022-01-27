835c83839d71815d6d0ac58a210bcca4bee61bb9
rasel <russelroy15@gmail.com>
2022-01-27 19:34:34 +0600

    WIP on restaurant project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
+import { RootState } from "./app/store";
+import ReservationCart from "./components/ReservationCart";
 
 function App() {
+  const reservations = useSelector(
+    (state: RootState) => state.reservations.value
+  );
   return (
     <div className="App">
       <div className="container">
         <div className="reservation-container">
           <div>
             <h5 className="reservation-header">Reservations</h5>
-            <div className="reservation-cards-container"></div>
+            <div className="reservation-cards-container">
+              {reservations.map((name) => {
+                return <ReservationCart name={name} />;
+              })}
+            </div>
           </div>
           <div className="reservation-input-container">
             <input />
